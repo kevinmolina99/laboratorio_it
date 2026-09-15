983ec286ec7dc60c547885ef79557ef787bca42b
Kevin Molina <alejandr_1999@hotmail.com>
2026-09-15 23:21:24 +0200

    esta es la prueba de stock de cajas acrilicas

---
+++ python/curso-python/modulo1/mi primera practica.py
@@ -0,0 +1,11 @@
+# practica de calculadora
+#quiero crear un programa que me ayude a calcular la venta de cajas acrilicas para mi negocio
+cajas=100
+venta=1
+stock= cajas-venta
+print(stock)
+
+
+
+
+
